db061a6e4676ae9632334ae0f9fae19f83216fde
BGrowth <benterprisesusa@gmail.com>
2026-07-02 18:08:42 -0300

    fix useEffect import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from './components/Navigation';
 import AppHub from './components/AppHub';
 import CleaningDash from './components/CleaningDash';
